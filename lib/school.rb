037a5ce3429589e78deee642c307a6683ea94b55
nick <nicholas_scalione@yahoo.com>
2021-01-20 16:50:30 -0500

    commit tree 42ef1ad6eb9d97344c82a41fb505ef342c6dbc1f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :roster
+
+
+def initialize(roster)
+    @roster = {}
+end
+
+def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+end
+
+def grade(name)
+    @roster[name]
+end
+
+def sort
+    @roster.each do |grade, name|
+      name.sort!
+
+    end
+end
+
+end
